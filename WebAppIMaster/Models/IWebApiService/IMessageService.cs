0dab82aaa71592598a7a00a0f57462172f1bad49
Sanzhar <Sanzhar@DESKTOP-LJHCV8H>
2019-06-29 11:50:06 +0600

    KcellServices

---
+++ WebAppIMaster/Models/IWebApiService/IMessageService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppIMaster.Models.IWebApiService
+{
+    public interface IMessageService
+    {
+          void PostKcell( int client_message_id, string sender, string recipient, string message_text );
+    }
+}
