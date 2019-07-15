0021f1a18bfb6272bdcb7ffd3a1951d12729ec72
Kaiyrbek_S <Kaiyrbek_S@KAIYRBEK_S>
2019-07-15 17:12:57 +0600

    commit

--- WebAppIMaster/Controllers/WebApi/MarketSerivecController.cs
+++ WebAppIMaster/Controllers/WebApi/MarketSerivecController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
 using WebAppIMaster.Models.Enitities;
+using WebAppIMaster.Models.WebApiModel;
 using WebAppIMaster.Models.WebApiService;
 using static WebAppIMaster.Models.WebApiModel.MarketServiceMdl;
 
@@ -15,8 +18,13 @@ namespace WebAppIMaster.Controllers.WebApi
     /// </summary>
     public class MarketSerivecController : ApiController
     {
-        // GET: api/MarketSerivece
-        public MarketDetails Get(int Id)
+        /// <summary>
+        /// api/GetDetailsMarket/{marketId} возвращает Market
+        /// </summary>
+        /// <param name="Id">Принимает параметр Id(marketId).</param>
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("api/GetDetailsMarket")]
+        public MarketDetails GetDetailsMarket(int Id)
         {
             ApplicationDbContext db = new ApplicationDbContext();
             MarketService repository = new MarketService(db);
@@ -25,11 +33,24 @@ namespace WebAppIMaster.Controllers.WebApi
         }
 
         /// <summary>
+        /// api/GetListMarket возвращает Market List
+        /// </summary>
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("api/GetListMarket")]
+        public List<MarketServiceMdl.MarketItem> GetListMarket()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            MarketService repository = new MarketService(db);
+            var model = repository.GetList();
+            return model;
+        }
+
+        /// <summary>
         /// передайте clientId,marketId(api/MarketSerivece/) сохранять в базу
         /// </summary>
         /// <param name="clientId">Принимает параметр clientId.</param>
         /// <param name="marketId">Принимает параметр marketId.</param>
-        // GET: api/MarketSerivece/5
+        [System.Web.Http.Route("api/Client")]
         public void PostClient( string clientId, int marketId )
         {
             ApplicationDbContext db = new ApplicationDbContext();
@@ -50,14 +71,69 @@ namespace WebAppIMaster.Controllers.WebApi
             repository.ClientPhone(executorId, marketId);
         }
 
-        // PUT: api/MarketSerivece/5
-        public void Put(int id, [FromBody]string value)
+        /// <summary>
+        /// api/MarketPhotosUrls/{marketId} возвращает List urls photo
+        /// </summary>
+        /// <param name="marketId">Принимает параметр marketId.</param>
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("api/MarketPhotosUrls")]
+        public List<string> MarketPhotosUrls(int marketId)
         {
+            ApplicationDbContext db = new ApplicationDbContext();
+            MarketService repository = new MarketService(db);
+            var result = repository.GetPhotosMarket(marketId);
+            return result;
         }
 
-        // DELETE: api/MarketSerivece/5
-        public void Delete(int id)
+
+        /// <summary>
+        /// api/GetPromotionAndDiscountPhotoForMarket/{marketId} возвращает Акция и скидки  url photo
+        /// </summary>
+        /// <param name="marketId">Принимает параметр marketId.</param>
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("api/GetPromotionAndDiscountPhotoForMarket")]
+        public string GetPromotionAndDiscountPhotoForMarket(int marketId)
         {
+            ApplicationDbContext db = new ApplicationDbContext();
+            MarketService repository = new MarketService(db);
+            var result = repository.GetPromotionAndDiscountPhotoForMarket(marketId);
+            return result;
+        }
+
+        /// <summary>
+        /// api/GetLogoTypeMarket/{marketId} возвращает Логотип маркет  url photo
+        /// </summary>
+        /// <param name="marketId">Принимает параметр marketId.</param>
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("api/GetLogoTypeMarket")]
+        public string GetLogoTypeMarket(int marketId)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            MarketService repository = new MarketService(db);
+            var result = repository.GetLogoTypeMarket(marketId);
+            return result;
+        }
+
+        /// <summary>
+        /// api/GetPhoto/{url} возвращает фото
+        /// </summary>
+        /// <param name="url">Принимает параметр url.</param>
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("api/GetPhoto")]
+        public HttpResponseMessage GetPhoto(string url)
+        {
+            if (url == "")
+            {
+                return null;
+            }
+            byte[] content = File.ReadAllBytes(HttpContext.Current.Server.MapPath(url));
+            MemoryStream ms = new MemoryStream(content);
+
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new StreamContent(ms);
+            response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("image/png");
+
+            return response;
         }
     }
 }
--- WebAppIMaster/Models/WebApiService/MarketService.cs
+++ WebAppIMaster/Models/WebApiService/MarketService.cs
@@ -9,14 +9,14 @@ using WebAppIMaster.Models.WebApiModel;
 
 namespace WebAppIMaster.Models.WebApiService
 {
-  
+
     public class MarketService : IMarketService
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        public MarketService( ApplicationDbContext db ) => this.db = db;
+        public MarketService(ApplicationDbContext db) => this.db = db;
 
-        public void ClientPhone( string clientId, int marketId )
+        public void ClientPhone(string clientId, int marketId)
         {
             Models.Enitities.ClientPhone clientPhone = new Models.Enitities.ClientPhone()
             {
@@ -28,7 +28,7 @@ namespace WebAppIMaster.Models.WebApiService
             db.SaveChanges();
         }
 
-        public void ExecutorPhone( string executorId, int marketId )
+        public void ExecutorPhone(string executorId, int marketId)
         {
             Models.ExecutorPhone executorPhone = new Models.ExecutorPhone()
             {
@@ -40,15 +40,133 @@ namespace WebAppIMaster.Models.WebApiService
             db.SaveChanges();
         }
 
-        public MarketServiceMdl.MarketDetails GetDetails( int id )
+        public MarketServiceMdl.MarketDetails GetDetails(int id)
         {
+            MarketServiceMdl.MarketDetails marketDetails = new MarketServiceMdl.MarketDetails();
             string langcode = LanguageController.CurrentCultureCode;
-            throw new NotImplementedException();
+            db.Configuration.AutoDetectChangesEnabled = false;
+            db.Configuration.LazyLoadingEnabled = false;
+
+            var item = (from o in db.Organizations
+                        from ol in o.Langs
+
+                        where ol.Langcode == langcode
+                        where o.Id == id
+                        select new
+                        {
+                            Id = o.Id,
+                            CategoryName = o.organizationCategories.Select(o => o.CategoryMarket).SelectMany(cm => cm.Langs).Where(l => l.Langcode == langcode).Select(l => l.CategoryName).FirstOrDefault(),
+                            CategoryId = o.organizationCategories.Select(o => o.CategoryMarket).SelectMany(cm => cm.Langs).Where(l => l.Langcode == langcode).Select(l => l.CategoryMarketId).FirstOrDefault(),
+                            RegionName = o.City.Langs.Where(l => l.Langcode == langcode).Select(l => l.Name).FirstOrDefault(),
+                            RegionId = o.CityId,
+                            Name = ol.Name,
+                            SiteUrl = o.SiteUrl,
+                            Email = o.Email,
+                            AboutCompany = ol.ShortDescription,
+                            Address = o.Address,
+                            SpecialtyName = o.organizationCategories.Select(o => o.CategoryMarket).SelectMany(cm => cm.organizationCategoryMarketInCategories).Select(occ => occ.Category).SelectMany(c => c.Langs).Where(l => l.Langcode == langcode).Select(l => l.Name).FirstOrDefault(),
+                            SpecialtyId = o.organizationCategories.Select(o => o.CategoryMarket).SelectMany(cm => cm.organizationCategoryMarketInCategories).Select(occ => occ.Category).SelectMany(c => c.Langs).Where(l => l.Langcode == langcode).Select(l => l.CategoryId).FirstOrDefault(),
+                            lattitude = o.latitude,
+                            Longitude = o.Longitude,
+                            PhoneNumber = o.PhoneNumber,
+                            ExtraPhoneNumber = o.ExtraPhoneNumber,
+                        }).SingleOrDefault();
+            var model = db.Organizations.Where(o => o.Id == id).SelectMany(o => o.iPOrganizationPrices).ToList();
+            Dictionary<string, string> service = new Dictionary<string, string>();
+            foreach (var price in model)
+            {
+                service.Add(price.ProductName, price.FromPrice + "-" + price.ToPrice == null ? " " : price.ToPrice + "." + price.TarifType.ToString());
+            }
+            List<string> phoneNumbers = new List<string>();
+            phoneNumbers.Add(item.PhoneNumber);
+            phoneNumbers.Add(item.ExtraPhoneNumber);
+            return new MarketServiceMdl.MarketDetails
+            {
+                Id = item.Id,
+                CategoryId = item.CategoryId,
+                CategoryName = item.CategoryName,
+                Name = item.Name,
+                RegionId = item.RegionId,
+                RegionName = item.RegionName,
+                SpecialtyId = item.SpecialtyId,
+                SpecialtyName = item.SpecialtyName,
+                SiteUrl = item.SiteUrl,
+                Email = item.Email,
+                Coordinates = item.lattitude + " " + item.Longitude,
+                Address = item.Address,
+                AboutCompany = item.AboutCompany,
+                ContactPhoneNumbers = phoneNumbers,
+                Serivces = service,
+            };
         }
 
         public List<MarketServiceMdl.MarketItem> GetList()
         {
-            throw new NotImplementedException();
+            string langcode = LanguageController.CurrentCultureCode;
+            db.Configuration.AutoDetectChangesEnabled = false;
+            db.Configuration.LazyLoadingEnabled = false;
+
+
+            var item = (from o in db.Organizations
+                        from ol in o.Langs
+
+                        where ol.Langcode == langcode
+
+                        select new MarketServiceMdl.MarketItem
+                        {
+                            Id = o.Id,
+                            CategoryName = o.organizationCategories.Select(o => o.CategoryMarket).SelectMany(cm => cm.Langs).Where(l => l.Langcode == langcode).Select(l => l.CategoryName).FirstOrDefault(),
+                            CategoryId = o.organizationCategories.Select(o => o.CategoryMarket).SelectMany(cm => cm.Langs).Where(l => l.Langcode == langcode).Select(l => l.CategoryMarketId).FirstOrDefault(),
+                            RegionName = o.City.Langs.Where(l => l.Langcode == langcode).Select(l => l.Name).FirstOrDefault(),
+                            RegionId = o.CityId,
+                            Address = o.Address,
+                            SpecialtyName = o.organizationCategories.Select(o => o.CategoryMarket).SelectMany(cm => cm.organizationCategoryMarketInCategories).Select(occ => occ.Category).SelectMany(c => c.Langs).Where(l => l.Langcode == langcode).Select(l => l.Name).FirstOrDefault(),
+                            SpecialtyId = o.organizationCategories.Select(o => o.CategoryMarket).SelectMany(cm => cm.organizationCategoryMarketInCategories).Select(occ => occ.Category).SelectMany(c => c.Langs).Where(l => l.Langcode == langcode).Select(l => l.CategoryId).FirstOrDefault(),
+                            Title = ol.Name,
+                        }).ToList();
+            return item;
+        }
+
+        public List<string> GetPhotosMarket(int marketId)
+        {
+            List<string> photos = new List<string>();
+            db.Configuration.AutoDetectChangesEnabled = false;
+            db.Configuration.LazyLoadingEnabled = false;
+            string langcode = LanguageController.CurrentCultureCode;
+
+            var model = db.Organizations.Where(o => o.Id == marketId).SelectMany(o => o.IPPhotosFiles).ToList();
+            foreach (var item in model)
+            {
+                photos.Add(item.PhotoUrl);
+            }
+
+            return photos;
+        }
+
+        public string GetPromotionAndDiscountPhotoForMarket(int marketId)
+        {
+            string PhotoUrl = " ";
+            db.Configuration.AutoDetectChangesEnabled = false;
+            db.Configuration.LazyLoadingEnabled = false;
+
+            var model = db.Organizations.Where(o => o.Id == marketId).SelectMany(o => o.PromotionAndDiscounts).ToList();
+            foreach (var item in model)
+            {
+                PhotoUrl = item.BannerUrl;
+            }
+            return PhotoUrl;
+        }
+
+        public string GetLogoTypeMarket(int marketId)
+        {
+            List<string> photos = new List<string>();
+            db.Configuration.AutoDetectChangesEnabled = false;
+            db.Configuration.LazyLoadingEnabled = false;
+            string langcode = LanguageController.CurrentCultureCode;
+
+            string LogotypeUrl = db.Organizations.Where(o => o.Id == marketId).Select(o => o.LogotypeUrl).FirstOrDefault();
+
+            return LogotypeUrl;
         }
     }
 }
